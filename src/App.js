055c1d22c996672f28753c24ee83af97f77ba9f0
seolleung2 <seolleung22@gmail.com>
2021-01-09 18:31:53 +0900

    make TodoList, TodoListItem Components, styled scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
