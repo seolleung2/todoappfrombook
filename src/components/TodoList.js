1a23168ebe7c88d95924cd33ded5cba2d37b14bb
seolleung2 <seolleung22@gmail.com>
2021-01-10 16:31:26 +0900

    implemented to remove todo things

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
+        <TodoListItem key={todo.id} todo={todo} onRemove={onRemove} />
       ))}
     </div>
   );
